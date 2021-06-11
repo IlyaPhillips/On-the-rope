5a4ab95418181308b9d347ac34b4574c123243f6
Ilya <ilya.phillips@gmail.com>
2021-06-11 23:24:39 +0100

    Basic Input

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -3,18 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.InputSystem;
 
 public class Knockback : MonoBehaviour
-{ 
+{
+    [SerializeField] private bool playerOne;
     [SerializeField] private Vector2 knockbackVector;
     [SerializeField] private GameObject target;
+    private PlayerControls _controls;
 
     private Rigidbody2D _rb, _targetRb;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _targetRb = target.GetComponent<Rigidbody2D>();
+        _controls = new PlayerControls();
+        _controls.PlayerOne.Attacks.performed += ctx => SendMessage();
+    }
+
+    private void SendMessage()
+    {
+        if (playerOne)
+        {
+            Debug.Log("Button Pressed");
+            _rb.AddForce(knockbackVector);
+        }
+    }
+
+    private void OnEnable()
+    {
+        _controls.PlayerOne.Enable();
+    }
+    private void OnDisable()
+    {    
+        _controls.PlayerOne.Disable();
     }
 
     // Update is called once per frame
@@ -25,13 +48,6 @@ public class Knockback : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
-    }
-
-    private void OnMouseDown()
-    {
-        
-        _rb.AddForce(knockbackVector);
         
     }
 }
