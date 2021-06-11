862218658c178866409bb49e3fe9814137f1a3ea
Ilya <ilya.phillips@gmail.com>
2021-06-11 21:48:36 +0100

    basic hit physics and rope

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Knockback : MonoBehaviour
+{ 
+    [SerializeField] private Vector2 knockbackVector;
+    [SerializeField] private GameObject target;
+
+    private Rigidbody2D _rb, _targetRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _targetRb = target.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+        _rb.AddForce(knockbackVector);
+        
+    }
+}
---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField] private Transform player, opponent;
+    private LineRenderer _line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        _line.startWidth = 0.2f;
+        _line.endWidth = 0.2f;
+        _line.startColor = Color.black;
+        _line.endColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPos = player.position;
+        var oppPos = opponent.position;
+        _line.SetPosition(0,playerPos);
+        _line.SetPosition(1,oppPos);
+        var dist = Vector2.Distance(playerPos, oppPos);
+        _line.startWidth = 0.4f / dist; 
+        _line.endWidth = 0.4f / dist; 
+    }
+}
